e4cb4df49b9bd551d3d20b6ce0610f5f59b257f7
eaeoz <sedatergoz@gmail.com>
2022-12-31 23:50:01 +0300

    sdf

--- shortner/urls.py
+++ shortner/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('create', views.create, name="create"),
-    path('<str:pk>', views.search, name="search")
+    path('<str:pk>/', views.search, name="search")
 ]
